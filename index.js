14a00152b23ab99b8398964e176108f534b00152
Serje <Serje.havandjian@gmail.com>
2022-04-25 18:18:02 -0400

    commit tree 64eb0424927d2845781623d32339ca6460244fc1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+let activity = "roller-skate"
+saturdayFun()
+
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork(activity)
+
+function wrapAdjective(adjective = "*"){
+    return function (paraDefault = "special"){
+        return `You are ${adjective}${paraDefault}${adjective}!`
+    }
+}
+
+wrapAdjective("Amazing")("Cool!")
